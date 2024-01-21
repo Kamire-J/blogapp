cfe7b106f5ea51b8d9d3918cfa5eebd8aabbd12d
Jack Kamire <jacmire@gmail.com>
2024-01-21 14:26:36 +0300

    routing and prisma implementation

--- src/components/categorylist/CategoryList.jsx
+++ src/components/categorylist/CategoryList.jsx
@@ -5,79 +5,43 @@ import Link from 'next/link'
 
 
 const getData = async()=>{
-  const res = await fetch("api/categories", {
+  const res = await fetch("http://localhost:3000/api/categories", {
     cache:"no-store",
   });
+  if(!res.ok){
+    throw new Error("Failed")
+  }
+
+  return res.json()
 };
 
-const CategoryList = () => {
+const CategoryList = async () => {
+  const data = await getData();
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Popular Topics</h1>
+      <h1 className={styles.title}>Popular Categories</h1>
       <div className={styles.categories}>
-          <Link href="/blog?cat=style" className={`${styles.category} ${styles.analytics}`}>
-            <Image  
-              src="/style.png" 
-              alt='' 
-              width={32} 
-              height={32} 
-              className={styles.image} 
-            />
-            Analytics
+        {data?.map((item) => (
+          <Link
+            href="/blog?cat=style"
+            className={`${styles.category} ${styles[item.slug]}`}
+            key={item._id}
+          >
+            {item.img && (
+              <Image
+                src={item.img}
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+            )}
+            {item.title}
           </Link>
-          <Link href="/blog" className={`${styles.category} ${styles.machine}`}>
-            <Image  
-              src="/fashion.png" 
-              alt='' 
-              width={32} 
-              height={32} 
-              className={styles.image} 
-            />
-            ML
-          </Link>
-          <Link href="/blog" className={`${styles.category} ${styles.frontend}`}>
-            <Image  
-              src="/coding.png" 
-              alt='' 
-              width={32} 
-              height={32} 
-              className={styles.image} 
-            />
-            frontend
-          </Link>
-          <Link href="/blog" className={`${styles.category} ${styles.backend}`}>
-            <Image  
-              src="/travel.png" 
-              alt='' 
-              width={32} 
-              height={32} 
-              className={styles.image} 
-            />
-            Backend
-          </Link>
-          <Link href="/blog" className={`${styles.category} ${styles.mobile}`}>
-            <Image  
-              src="/culture.png" 
-              alt='' 
-              width={32} 
-              height={32} 
-              className={styles.image} 
-            />
-            Mobile
-          </Link>
-          <Link href="/blog" className={`${styles.category} ${styles.games}`}>
-            <Image  
-              src="/culture.png" 
-              alt='' 
-              width={32} 
-              height={32} 
-              className={styles.image} 
-            />
-            Games
-          </Link>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryList
+export default CategoryList;
