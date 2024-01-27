9e070b5a5f8673d1cc03bdaf2c660ea8d899a3ef
Jack Kamire <jacmire@gmail.com>
2024-01-27 21:25:45 +0300

    routing and api endpoints

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -3,64 +3,50 @@ import styles from './singlePage.module.css'
 import Image from 'next/image'
 import Comments from '@/components/comments/Comments'
 
-const SinglePage = () => {
+
+const getData = async (slug) => {
+    const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+      cache: "no-store",
+    });
+  
+    if (!res.ok) {
+      throw new Error("Failed");
+    }
+  
+    return res.json();
+  };
+
+
+const SinglePage = async({params}) => {
+    const {slug} = params;
+
+    const data = await getData(slug)
   return (
     <div className={styles.container}>
         <div className={styles.infoContainer}>
             <div className={styles.textContainer}>
-                <h1 className={styles.title}>What is Lorem Ipsum?</h1>
+                <h1 className={styles.title}>{data?.title}</h1>
                 <div className={styles.user}>
-                    <div className={styles.userImageContainer}>
-                        <Image src="/p1.jpeg" alt='' fill className={styles.avatar} />   
-                    </div>
+                    {data?.img && (<div className={styles.userImageContainer}>
+                        <Image src={data?.user.image} alt='' fill className={styles.avatar} />   
+                    </div>)}
                     <div className={styles.userTextContainer}>
-                        <span className={styles.username}>Jane Doe</span>
-                        <span className={styles.date}>31.12.2023</span>
+                        <span className={styles.username}>{data?.user.name}</span>
+                        <span className={styles.date}>{data?.createdAt.substring(0,10)}</span>
                     </div>
                 </div>
             </div>
-            <div className={styles.imageContainer}>
-                <Image src="/p1.jpeg" alt='' fill className={styles.image}/>
-            </div>
+            {data?.img && (
+                <div className={styles.imageContainer}>
+                    <Image src={data.img} alt="" fill className={styles.image} />
+                </div>
+        )}
         </div>
         <div className={styles.content}>
             <div className={styles.post}>
-                <div className={styles.description}>
-                    <p>
-                    Lorem Ipsum is simply dummy text of the printing and 
-                    typesetting industry. Lorem Ipsum has been the industry's 
-                    standard dummy text ever since the 1500s, when an unknown 
-                    printer took a galley of type and scrambled it to make a type 
-                    specimen book. It has survived not only five centuries, 
-                    but also the leap into electronic typesetting, remaining 
-                    essentially unchanged. It was popularised in the 1960s with 
-                    the release of Letraset sheets containing Lorem Ipsum passages, 
-                    and more recently with desktop publishing software like Aldus 
-                    PageMaker including versions of Lorem Ipsum.
-                    </p>
-
-                    <h3>Where does it come from?</h3>
-
-                    <p>
-                    Contrary to popular belief, Lorem Ipsum is not simply 
-                    random text. It has roots in a piece of classical Latin 
-                    literature from 45 BC, making it over 2000 years old. 
-                    Richard McClintock, a Latin professor at Hampden-Sydney 
-                    College in Virginia, looked up one of the more obscure 
-                    Latin words, consectetur, from a Lorem Ipsum passage, 
-                    and going through the cites of the word in classical 
-                    literature, discovered the undoubtable source. 
-                    Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 
-                    "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) 
-                    by Cicero, written in 45 BC. This book is a treatise on the 
-                    theory of ethics, very popular during the Renaissance. 
-                    The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", 
-                    comes from a line in section 1.10.32.
-
-                    </p>
-                </div>
+                <div className={styles.description} dangerouslySetInnerHTML={ { __html: `${data?.desc}` }} />
                 <div className={styles.comment}>
-                    <Comments />
+                    <Comments postSlug={slug} />
                 </div>
             </div>
             <Menu/>
